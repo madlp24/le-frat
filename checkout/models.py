0d0bd2dc031eb8803c11fce1e0ff7b6047c70b54
madlp24 <120478064+madlp24@users.noreply.github.com>
2026-01-14 17:42:04 -0500

    Add order and order line item models for checkout

--- checkout/models.py
+++ checkout/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import User
+from products.models import Product
 
-# Create your models here.
+
+class Order(models.Model):
+    order_number = models.CharField(max_length=32, null=False, editable=False, unique=True)
+    user = models.ForeignKey(User, null=True, blank=True, on_delete=models.SET_NULL)
+    full_name = models.CharField(max_length=80)
+    email = models.EmailField()
+    phone_number = models.CharField(max_length=20)
+    street_address1 = models.CharField(max_length=80)
+    street_address2 = models.CharField(max_length=80, blank=True)
+    town_or_city = models.CharField(max_length=40)
+    postcode = models.CharField(max_length=20, blank=True)
+    country = models.CharField(max_length=40)
+
+    date = models.DateTimeField(auto_now_add=True)
+    order_total = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+
+    def __str__(self):
+        return self.order_number
+
+
+class OrderLineItem(models.Model):
+    order = models.ForeignKey(Order, null=False, blank=False, related_name="lineitems", on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, null=False, blank=False, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+    lineitem_total = models.DecimalField(max_digits=10, decimal_places=2, editable=False)
+
+    def __str__(self):
+        return f"{self.product.name} on order {self.order.order_number}"
