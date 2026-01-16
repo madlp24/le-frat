2b78f18e9e50838dfe7baf50ef2e986a537f5819
madlp24 <120478064+madlp24@users.noreply.github.com>
2026-01-16 17:00:08 -0500

    Add profile page with user order history

--- lefrat/settings.py
+++ lefrat/settings.py
@@ -48,7 +48,7 @@ INSTALLED_APPS = [
     "products",
     "bag",
     "checkout",
-    "profiles",
+    "profiles.apps.ProfilesConfig",
 ]
 
 MIDDLEWARE = [
--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,8 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from checkout.models import Order
 
-# Create your views here.
+@login_required
+def profile(request):
+    orders = Order.objects.filter(user=request.user).order_by("-date")
+    return render(request, "profiles/profile.html", {"orders": orders})
