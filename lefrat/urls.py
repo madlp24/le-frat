ce2743d0ea62f5d759a80e42551e74797a3979d0
madlp24 <120478064+madlp24@users.noreply.github.com>
2026-01-19 18:15:46 -0500

    Enable sitemap.xml using Django sitemaps

--- lefrat/urls.py
+++ lefrat/urls.py
@@ -16,6 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
+from django.contrib.sitemaps.views import sitemap
+from .sitemaps import StaticViewSitemap
+
+sitemaps = {
+    'static': StaticViewSitemap,
+}
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -25,4 +32,8 @@ urlpatterns = [
     path("bag/", include("bag.urls")),
     path("checkout/", include("checkout.urls")),
     path("profile/", include("profiles.urls")),
+    
+    path("sitemap.xml", sitemap, {"sitemaps": sitemaps}, name="sitemap"),
+
+    path("robots.txt", TemplateView.as_view(template_name="robots.txt", content_type="text/plain")),
 ]
